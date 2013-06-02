7320e7536847135432959c89372f94d972441b76
sinojelly <sinojelly@gmail.com>
2013-06-02 11:58:26 +0800

    1. ignore some files in deploy. 2. update help.txt

--- WizSheet/deploy.bat
+++ WizSheet/deploy.bat
@@ -1,2 +1,2 @@
 set path=C:\Program Files\WinRAR;%path%
-winrar a -r -afzip -xdeploy.bat WizSheet.wiztemplate .
+winrar a -r -afzip -xdeploy.bat -xwizsheet\jquery.sheet\examples -xwizsheet\jquery.sheet\jquery-1.8.3.js WizSheet.wiztemplate .
